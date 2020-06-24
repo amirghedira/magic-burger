325fa64509117fb223fb90c17553b8cda3dddf0e
amir ghedira <amirghedira06@gmail.com>
2020-06-24 19:35:11 +0100

    routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route, Redirect } from 'react-router-dom'
+import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom'
 
 import './App.css';
 import 'antd/dist/antd.css'
@@ -7,25 +7,26 @@ import 'antd/dist/antd.css'
 import Toolbar from './components/Toolbar/Toolbar'
 import BurgerBuilder from './components/BurgerBuilder/BurgerBuilder'
 import MyOrders from './components/MyOrders/MyOrders'
-import { Switch } from 'antd';
 
 function App() {
     return (
 
         <BrowserRouter>
             <Toolbar />
-            <Route
-                path="/"
-                exact
-                render={() => <BurgerBuilder />}
+            <Switch>
+                <Route
+                    path="/"
+                    exact
+                    render={() => <BurgerBuilder />}
 
-            />
-            <Route
-                path="/orders"
-                render={() => <MyOrders />}
+                />
+                <Route
+                    path="/orders"
+                    render={() => <MyOrders />}
 
-            />
-            <Redirect to="/" />
+                />
+                <Redirect to="/" />
+            </Switch>
         </BrowserRouter >
     );
 }
